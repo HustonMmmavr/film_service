1fd8bf3c3fb81d5268c3cddf075087b540766af0
HustonMmmavr <huston.mavr@gmail.com>
2017-12-05 19:26:33 +0300

    need to make controller

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -1,4 +1,13 @@
 class FilmController < ApplicationController
+  @@important_params = ['filmTitle', 'filmAbout', 'filmLength', 'filmYear', 'filmDirector']
+  @@all_params = ['filmTitle', 'filmAbout', 'filmLength', 'filmYear', 'filmDirector',
+                'filmImage', 'filmRating']
+  @@hash_global_and_local = {'filmTitle'=>'title', 'filmAbout'=>'about',
+      'filmLength'=>'length', 'filmYear'=>'year', 'filmDirector'=>'director',
+      'filmImage'=>'image', 'filmRating'=>'rating'}
+  @@int_regexp = /^\d+$/
+
+
   def is_parameter_valid(param_name, param, regexp = nil)
     if param == nil || param == ""
         return param_name + " is Empty"
@@ -11,17 +20,31 @@ class FilmController < ApplicationController
     true
   end
 
-  def init_params()
-    @important_params = ['filmTitle', 'filmAbout', 'filmLength', 'filmYear', 'filmDirector']
-    @all_params = ['filmTitle', 'filmAbout', 'filmLength', 'filmYear', 'filmDirector',
-                  'filmImage', 'filmRating']
+  def get_request_film()
+    film = {}
+    temp = @film.slice(:id, :name, :email, :password, :avatar)
+    temp.each do |key, value|
+      user[@@hash_local_and_global.key(key)] = value
+    end
+    film
+  end
+
+  def params_to_db_params(params)
+    db_params = {}
+    @@all_params.each do |key|
+      db_params[@@hash_local_and_global[key]] = params[key]
+    end
+    db_params
+  end
+
+  def get_request_films()
+
   end
 
   def create_film()
-    init_params
-    @important_params.each do |key|
+    @@important_params.each do |key|
       if key == 'filmLength' || key = 'filmYear'
-        check = is_parameter_valid key, param[key], @int_regexp
+        check = is_parameter_valid key, param[key], @@int_regexp
       else
         check = is_parameter_valid key, param_key, nil
       end
@@ -30,14 +53,13 @@ class FilmController < ApplicationController
       end
     end
 
-    film = Film.new()
+    film = Film.new(params_to_db_params(params))
     if film.valid?
     begin
       film.save()
-      responseMessage = ResponseMessage.new("Ok")
       return render :json => {:respMsg => "Ok"}, :status => 201
     rescue
-        responseMessage = ResponseMessage.new("Database error")
+        responseMessage = ReVsponseMessage.new("Database error")
         return render :json => responseMessage, :status => 500
     end
   end
@@ -51,28 +73,40 @@ class FilmController < ApplicationController
     id = params[:id]
     check_film_id = is_parameter_valid 'id', id, @int_regexp
     if check_film_id != true
-        return render :json => {:respMsg => check_film_id}, :status => 400
+      return render :json => {:respMsg => check_film_id}, :status => 400
     end
 
     begin
         @film = Film.find_by_id(id)
         if @film == nil
-            responseMessage = ResponseMessage.new("No such film")
-            return render :json => responseMessage, :status => 404
+          responseMessage = ResponseMessage.new("No such film")
+          return render :json => responseMessage, :status => 404
         end
     rescue
-        responseMessage = ResponseMessage.new("Database error")
-        return render :json => responseMessage, :status => 500
+      responseMessage = ResponseMessage.new("Database error")
+      return render :json => responseMessage, :status => 500
     end
-    #not this render
-    film = fill_film
-    render :json => {'respMsg': 'Ok', 'film': film}
-    #render :json => @film#, :only => ['id', "avatar"]
+    render :json => {'respMsg': 'Ok', 'film': get_request_film}
   end
 
-  #return json arr
+
+
   def get_films()
-    id = params[:id]
+    param_names = ['page', 'count']
+    param_names.each do |key|
+      check = is_parameter_valid params[key], key, @@int_regexp
+      if check != true
+        return render :json => {:respMsg => check}, :status => 400
+      end
+    end
+
+    count = Integer(params[:count])
+    offset = Integer(params[:page])*count
+
+    begin
+
+    rescue
+    end
   end
 
   def delete_film()
@@ -81,13 +115,25 @@ class FilmController < ApplicationController
     if check_film_id != true
         return render :json => {:respMsg => check_film_id}, :status => 400
     end
+
+    begin
+      film = Film.find_by_id(id)
+      if film != nil
+        Film.destroy(id)
+        return render :json {:respMsg => "Ok"}, :status => 200
+      end
+      return render :json => {:respMsg => "No such film"}, :status => 404
+    rescue
+      responseMessage = ResponseMessage.new("Database error")
+      return render :json => responseMessage, :status => 500
+    end
   end
 
   def update_film()
     init_params
     update_fields = []
     @all_params.each do |key|
-      key = 
+      key =
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/get_film/:id', to 'film#get_film', as :get_film
+  get '/get_films/:page/:count', to 'film#get_films', as :get_films
+  post '/add_film/', to 'film#create_film', as :add_film
+  delete '/delete_film/', to 'film#delete_film', as :delete_film
+  post '/update_film/', to 'film#update_film', as :update_film
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
